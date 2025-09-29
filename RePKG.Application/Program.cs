4fc3cc70e551593b6814e6a7f8eee845d8f29c38
yang369chun <159139760+yang369chun@users.noreply.github.com>
2025-09-30 00:21:56 +0800

    Update Program.cs

--- RePKG.Application/Program.cs
+++ RePKG.Application/Program.cs
@@ -1,1 +1,13 @@
+using System;
 
+namespace RePKG.Application
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            // 调用 RePKG 核心逻辑
+            RePKG.MainApp.Run(args);
+        }
+    }
+}
